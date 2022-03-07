a4a8c9d7ac2e3d03ab03ee402743b391a70b965d
6asm6deus6 <6asm6deus6@gmail.com>
2022-03-07 20:18:18 +0500

    little rework v.2

---
+++ main.js
@@ -0,0 +1,14 @@
+let adviceId = document.querySelector('.title');
+let adviceText = document.querySelector('.text');
+
+async function responseFunc() {
+  let response = await fetch('https://api.adviceslip.com/advice');
+  let data = await response.json();
+
+  let slipObj = data.slip;
+  let {id, advice} = slipObj
+
+  adviceId.textContent = 'Advice #' + id;
+  adviceText.innerHTML = `"${advice}"`;
+};
+responseFunc();
