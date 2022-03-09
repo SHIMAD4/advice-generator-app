a46462be90b538a5c31a3013099c34497bcd23e8
6asm6deus6 <6asm6deus6@gmail.com>
2022-03-09 17:25:05 +0500

    removed the cache = speeded up the site

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ let adviceId = document.querySelector('.title');
 let adviceText = document.querySelector('.text');
 
 async function responseFunc() {
-  let response = await fetch('https://api.adviceslip.com/advice');
+  let response = await fetch('https://api.adviceslip.com/advice', {
+    method: "GET",
+    cache: "no-cache"
+  });
   let data = await response.json();
 
   let slipObj = data.slip;
-  let {id, advice} = slipObj
+  let {
+    id,
+    advice
+  } = slipObj
 
   adviceId.textContent = 'Advice #' + id;
   adviceText.innerHTML = `"${advice}"`;
